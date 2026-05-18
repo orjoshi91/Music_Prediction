e1b692860a92f9ee8cda862e31b265fbee35d193
orjoshi91 <orjoshi@ucsd.edu>
2026-05-17 22:24:07 -0700

    model training

--- src/train.py
+++ src/train.py
@@ -1,2 +1,49 @@
-def train_model():
-    return x
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras.callbacks import EarlyStopping
+
+def train_model(X_train, X_test, y_train, y_test, num_classes):
+
+    model = Sequential([
+        Input(shape=(X_train.shape[1],)),
+
+        Dense(128, activation='relu'),
+        Dropout(0.3),
+
+        Dense(64, activation='relu'),
+        Dropout(0.3),
+
+        Dense(32, activation='relu', name='embedding_layer'),
+
+        Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    early_stop = EarlyStopping(
+        monitor='val_loss',
+        patience=3,
+        restore_best_weights=True
+    )
+
+    print("\nTraining model...\n")
+
+    history = model.fit(
+        X_train,
+        y_train,
+        validation_data=(X_test, y_test),
+        epochs=20,
+        batch_size=64,
+        callbacks=[early_stop],
+        verbose=1
+    )
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+
+    print("\nTest Accuracy:", accuracy)
+
+    return model, history

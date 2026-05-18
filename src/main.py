7b5f03dc3331540d7535c026ed6ec184a0ef32d4
orjoshi91 <orjoshi@ucsd.edu>
2026-05-17 22:26:33 -0700

    model training

--- src/main.py
+++ src/main.py
@@ -8,6 +8,15 @@ def main():
     df, feature_cols = load_dataset("data/dataset.csv")
     X_train, X_test, y_train, y_test, scalar, encoder = preprocess_data(df, feature_cols)
     # model = train_model(processed_data)
+    num_classes = len(encoder.classes_)
+
+    model, history = train_model(
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+        num_classes
+    )
     # recommendations = recommend_tracks(model, processed_data)
     #
     # print(recommendations)

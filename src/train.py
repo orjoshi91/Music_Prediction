318bbc85cb39bba860381cf976082a04688a6025
orjoshi91 <orjoshi@ucsd.edu>
2026-05-18 11:46:11 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,53 +1,37 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Input
-from tensorflow.keras.callbacks import EarlyStopping
-
+from tensorflow.keras.layers import Dense
 
 def train_model(X_train, X_test, y_train, y_test, num_classes):
 
-    # neural network
-    model = Sequential([
-        Input(shape=(X_train.shape[1],)),
+    model = Sequential()
 
-        Dense(128, activation='relu'),
-        Dropout(0.2),
+    # Input layer
+    model.add(Dense(64, activation='relu', input_shape=(X_train.shape[1],)))
 
-        Dense(64, activation='relu', name='embedding_layer'),
+    model.add(Dense(32, activation='relu'))
 
-        Dense(num_classes, activation='softmax')
-    ])
+    # Output layer
+    model.add(Dense(num_classes, activation='softmax'))
 
-    # Compiling the model
     model.compile(
         optimizer='adam',
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
 
-    # Stoping the training early if validation loss stops improving
-    early_stop = EarlyStopping(
-        monitor='val_loss',
-        patience=3,
-        restore_best_weights=True
-    )
+    print("Training model...\n")
 
-    print("\nTraining model...\n")
-
-    # Training
     history = model.fit(
         X_train,
         y_train,
-        validation_data=(X_test, y_test),
-        epochs=20,
+        epochs=10,
         batch_size=64,
-        callbacks=[early_stop],
-        verbose=1
+        validation_data=(X_test, y_test)
     )
 
-    # Evaluating performance
-    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+    loss, accuracy = model.evaluate(X_test, y_test)
 
-    print("\nTest Accuracy:", accuracy)
+    print("Test Accuracy:", accuracy)
 
     return model, history
